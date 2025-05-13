03fecf3771ed62b55fa95c6a39841aafb50493f5
Vasilis Nicolaou <vasilis.nicolaou@vaslabs.io>
2025-05-13 15:49:32 +0300

    New MathOps feature: multiply two numbers

--- src/main/scala/MathOps.scala
+++ src/main/scala/MathOps.scala
@@ -1,2 +1,3 @@
 object MathOps:
     def addTwoNumbers(a: Int, b: Int) = a + b
+    def multiplyTwoNumbers(a: Int, b: Int) = a * b
--- src/test/scala/MathOpsSpec.scala
+++ src/test/scala/MathOpsSpec.scala
@@ -4,6 +4,14 @@ import matchers._
 
 class MathOpsSpec extends AnyFlatSpec with must.Matchers {
   "Adding 5 and 8" must "give 13" in {
-    MathOps.addTwoNumbers(5, 8)
+    MathOps.addTwoNumbers(5, 8) mustEqual 13
+  }
+
+  "Multiplying 5 and 8" must "give 40" in {
+    MathOps.multiplyTwoNumbers(5, 8) mustEqual 40
+  }
+
+  "Multiplying 7 and 9" must "give 63" in {
+    MathOps.multiplyTwoNumbers(7, 9) mustEqual 63
   }
 }
